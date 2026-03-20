1b6456a74c17e5a9f6e1bb460d4b19212d8961b6
Luca Crinò <crinoluca@gmail.com>
2026-03-20 20:18:48 +0100

    Update A2_revenue_by_country.sql

--- sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
+++ sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
@@ -1,2 +1,9 @@
 --Total revenue and average order value by country and loyalty tier. Which combination generates the highest revenue per order?
 
+select country, loyalty_tier, round(sum(line_total),2) as total_revenue, count(*) as total_orders, round(avg(line_total),2) as avg_order_value
+from customers c
+natural join orders o
+left join order_items oi
+on o.order_id = oi.order_id
+group by 1,2
+order by 1, array_position(array['platinum','gold','silver','bronze'],loyalty_tier)
