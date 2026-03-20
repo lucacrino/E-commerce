6c33482f8ba3291858d14f49b0ddb59456b77aab
Luca Crinò <crinoluca@gmail.com>
2026-03-20 19:58:30 +0100

    Update A2_revenue_by_country.sql

--- sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
+++ sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
@@ -1,2 +1,2 @@
---Total revenue and average order value by: customer, country and loyalty tier. Which combination generates the highest revenue per order?
+--Total revenue and average order value by country and loyalty tier. Which combination generates the highest revenue per order?
 
