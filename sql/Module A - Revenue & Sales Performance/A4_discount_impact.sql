dc411d7959e03d8bf6801b33f61c5117f7bf9d30
Luca Crinò <crinoluca@gmail.com>
2026-03-21 15:12:48 +0100

    Update A4_discount_impact.sql

--- sql/Module A - Revenue & Sales Performance/A4_discount_impact.sql
+++ sql/Module A - Revenue & Sales Performance/A4_discount_impact.sql
@@ -1,1 +1,2 @@
+-- For each discount tier (0%, 5–10%, 15–20%, 25%+), let's check the number of orders, total revenue and average number of items per order. Does heavier discounting correlate with larger basket sizes?
 
