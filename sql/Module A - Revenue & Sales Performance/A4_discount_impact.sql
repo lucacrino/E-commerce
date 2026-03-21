68f859b0fb1456cd8a807076ae5f5db2d3430248
Luca Crinò <crinoluca@gmail.com>
2026-03-21 22:24:29 +0100

    Update A4_discount_impact.sql

--- sql/Module A - Revenue & Sales Performance/A4_discount_impact.sql
+++ sql/Module A - Revenue & Sales Performance/A4_discount_impact.sql
@@ -1,4 +1,4 @@
---Objective: For each discount tier (0%, 5–10%, 15–20%, 25%), let's check the number of orders, total revenue and average number of items per order. Does heavier discounting correlate with larger basket sizes?
+--Objective: for each discount tier (0%, 5–10%, 15–20%, 25%), let's check the number of orders, total revenue and average number of items per order. Does heavier discounting correlate with larger basket sizes?
 
 with order_discount_cte as(
 select o.order_id, trunc(coalesce(discount_pct,0)) || '%' as discount_bucket, round(sum(line_total),2) as revenue, count(order_item_id) as items_ordered
