6a9e5c719153b14865199bd3b3c2bbade0d2c1e8
Luca Crinò <crinoluca@gmail.com>
2026-03-21 15:04:49 +0100

    Update A3_monthly_rolling_avg.sql

--- sql/Module A - Revenue & Sales Performance/A3_monthly_rolling_avg.sql
+++ sql/Module A - Revenue & Sales Performance/A3_monthly_rolling_avg.sql
@@ -1,2 +1,13 @@
 --3 month rolling average of gross revenue. This smooths out seasonal spikes and reveals underlying growth trends.
 
+with monthly_revenue_cte as(
+select to_char(o.delivery_date,'Mon') as month_name, round(sum(line_total),2) as monthly_revenue
+from orders o
+natural join order_items oi
+where delivery_date is not null
+group by to_char(o.delivery_date,'Mon'), extract(month from o.delivery_date)
+order by extract(month from o.delivery_date)
+)
+
+select *, round(avg(monthly_revenue) over(rows between 2 preceding and current row),2) as rolling_3m_avg
+from monthly_revenue_cte
