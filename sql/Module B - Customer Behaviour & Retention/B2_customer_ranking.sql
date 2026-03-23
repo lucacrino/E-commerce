69c8589c8297a63d6234e173c90297322c63fac8
Luca Crinò <crinoluca@gmail.com>
2026-03-23 18:57:35 +0100

    Update B2_customer_ranking.sql

--- sql/Module B - Customer Behaviour & Retention/B2_customer_ranking.sql
+++ sql/Module B - Customer Behaviour & Retention/B2_customer_ranking.sql
@@ -1,17 +1,19 @@
 --Objective: For each country, rank customers by total spend and identify the top 3 spenders. Also compute each customer's share of their country's total revenue.
 
 with country_ranking_cte as(
-select country, 
-first_name || ' ' || last_name as full_name, 
-sum(line_total) as total_spend, 
-dense_rank() over(partition by country order by sum(line_total) desc) as country_rank
-from orders o
-inner join customers c           
-on o.customer_id = c.customer_id 
-inner join order_items oi
-on o.order_id = oi.order_id
-where status = 'delivered'
-group by 1,2
+    select 
+	country, 
+    first_name || ' ' || last_name as full_name, 
+    sum(line_total) as total_spend, 
+    dense_rank() over(partition by country order by sum(line_total) desc) as country_rank
+    from 
+	orders o
+    inner join customers c           
+    on o.customer_id = c.customer_id 
+    inner join order_items oi
+    on o.order_id = oi.order_id
+    where status = 'delivered'
+    group by 1,2
 )
 
 
