8e062a1beb952a5b5e08dcbe30e169113c209244
Luca Crinò <crinoluca@gmail.com>
2026-03-23 10:37:08 +0100

    Update B1_customer_lifetime.sql

--- sql/Module B - Customer Behaviour & Retention/B1_customer_lifetime.sql
+++ sql/Module B - Customer Behaviour & Retention/B1_customer_lifetime.sql
@@ -1,3 +1,5 @@
 --Objective: Rank all customers by their total spend across delivered orders. Include their country, loyalty tier and number of orders placed. Show the top 20 customers globally.
---Note: ~40 orders reference customer_ids absent from customers
+--Note: ~40 orders reference customer_ids absent from customers. 
+
+
 
