7d3bcbc0b357540d5904945b3529fb4b2bab8b73
Luca Crinò <crinoluca@gmail.com>
2026-03-23 09:27:53 +0100

    Update A1_monthly_revenue.sql

--- sql/Module A - Revenue & Sales Performance/A1_monthly_revenue.sql
+++ sql/Module A - Revenue & Sales Performance/A1_monthly_revenue.sql
@@ -1,5 +1,5 @@
 --Objective: what is the month-by-month gross revenue, number of orders and average order value over the entire dataset period?
---Note: according to the Accrual Accounting principles revenues are recognized when the good/service is earned (delivery_date) and not when cash is paid (order_date). This standard will be adopted across the whole project.         
+--Note: according to the Accrual Accounting principles revenues are recognized when the good/service is earned (delivery_date is not null) and not when cash is paid (order_date). This standard will be adopted across the whole project.         
 
 select to_char(o.delivery_date,'Mon'), count(*) as total_orders, round(sum(quantity * unit_price),2) as gross_revenue, round(avg(line_total),2) as avg_order_value
 from orders o
