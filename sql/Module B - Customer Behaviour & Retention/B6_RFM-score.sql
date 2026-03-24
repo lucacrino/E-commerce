cd0c4a38048fe3e06b24c006574ece4ca78ea59c
Luca Crinò <crinoluca@gmail.com>
2026-03-24 10:22:08 +0100

    Create B6_RFM-score.sql

---
+++ sql/Module B - Customer Behaviour & Retention/B6_RFM-score.sql
@@ -0,0 +1,2 @@
+--Objective: Compute an RFM (Recency, Frequency, Monetary) score for each customer. Recency = days since last order, Frequency = number of delivered orders, Monetary = total spend. Assign a score of 1-4 to each dimension, then classify customers into segments such as Champions, Loyal, At Risk, and Lost.
+
