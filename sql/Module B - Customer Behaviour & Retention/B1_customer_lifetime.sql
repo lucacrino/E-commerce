20b8d7e5416e29803fc519f9f8fa5cde6d012c0a
Luca Crinò <crinoluca@gmail.com>
2026-03-26 18:42:04 +0100

    Update B1_customer_lifetime.sql

--- sql/Module B - Customer Behaviour & Retention/B1_customer_lifetime.sql
+++ sql/Module B - Customer Behaviour & Retention/B1_customer_lifetime.sql
@@ -6,12 +6,9 @@ c.customer_id,
 first_name || ' ' || last_name as full_name, country, 
 loyalty_tier, sum(line_total) as total_spend, 
 count(*) as order_count
-from 
-orders o
-inner join customers c             --Remove customer_ids not in customers
-on o.customer_id = c.customer_id 
-inner join order_items oi
-on o.order_id = oi.order_id
+from orders o
+inner join customers c on o.customer_id = c.customer_id    --Remove customer_ids not in customers 
+inner join order_items oi on o.order_id = oi.order_id
 where status = 'delivered'
 group by 1,2,3,4
 order by total_spend desc
