df9bb0f20b979efefee4165168de129526e439b1
Luca Crinò <crinoluca@gmail.com>
2026-03-26 18:36:58 +0100

    Update B1_customer_lifetime.sql

--- sql/Module B - Customer Behaviour & Retention/B1_customer_lifetime.sql
+++ sql/Module B - Customer Behaviour & Retention/B1_customer_lifetime.sql
@@ -2,24 +2,24 @@
 --Note: ~40 orders reference customer_ids absent from customers. 
 
 select 
-    c.customer_id, 
-    first_name || ' ' || last_name as full_name, country, 
-    loyalty_tier, sum(line_total) as total_spend, 
-    count(*) as order_count
+c.customer_id, 
+first_name || ' ' || last_name as full_name, country, 
+loyalty_tier, sum(line_total) as total_spend, 
+count(*) as order_count
 from 
-    orders o
-    inner join customers c             --Remove customer_ids not in customers
-    on o.customer_id = c.customer_id 
-    inner join order_items oi
-    on o.order_id = oi.order_id
+orders o
+inner join customers c             --Remove customer_ids not in customers
+on o.customer_id = c.customer_id 
+inner join order_items oi
+on o.order_id = oi.order_id
 where 
-    status = 'delivered'
+status = 'delivered'
 group by 
-    1,2,3,4
+1,2,3,4
 order by 
-    total_spend desc
+total_spend desc
 limit 
-    20
+20
 
 
 
