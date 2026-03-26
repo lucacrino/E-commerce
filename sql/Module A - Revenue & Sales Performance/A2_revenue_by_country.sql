beeecf32f2a21b89d0360fcdd6a18a4196b8ab66
Luca Crinò <crinoluca@gmail.com>
2026-03-26 18:35:01 +0100

    Update A2_revenue_by_country.sql

--- sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
+++ sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
@@ -5,10 +5,13 @@ loyalty_tier,
 round(sum(line_total),2) as total_revenue, 
 count(distinct o.order_id) as total_orders, 
 round(avg(line_total),2) as avg_order_value
-from customers c
+from 
+customers c
 natural join orders o
-left join order_items oi
-on o.order_id = oi.order_id
-where delivery_date is not null  
-group by 1,2
-order by 1, array_position(array['platinum','gold','silver','bronze'],loyalty_tier)
+left join order_items oi on o.order_id = oi.order_id
+where 
+delivery_date is not null  
+group by 
+1,2
+order by 
+1, array_position(array['platinum','gold','silver','bronze'],loyalty_tier)
