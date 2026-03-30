29680b567b94f0d96b3b739bf36e5e68818423b1
Luca Crinò <crinoluca@gmail.com>
2026-03-30 21:10:20 +0200

    Create C5_shipping_method_efficiency.sql

---
+++ sql/Module C - Production & Operations/C5_shipping_method_efficiency.sql
@@ -0,0 +1,2 @@
+--Objective: For each shipping method, calculate: average days from order to delivery (for delivered orders), percentage of orders that were returned, average shipping cost, and total revenue. Does express shipping reduce return rates?
+
