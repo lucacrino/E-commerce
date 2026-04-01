11f0149bdf7b42148188b8d5b46436c9f8554d90
Luca Crinò <crinoluca@gmail.com>
2026-04-01 22:12:56 +0200

    Create C7_inventory_risk.sql

---
+++ sql/Module C - Production & Operations/C7_inventory_risk.sql
@@ -0,0 +1,2 @@
+--Objective: Identify products where stock_quantity is above the 75th percentile for their category, but units sold in the last 6 months are below the 25th percentile. These are slow-moving, high-inventory risk items. Include the estimated stock value (stock_quantity * cost_price). 
+
