1f47ea293334167021d580d6e8a7ed0434f59224
Luca Crinò <crinoluca@gmail.com>
2026-04-07 18:20:37 +0200

    Update C7_inventory_risk.sql

--- sql/Module C - Production & Operations/C7_inventory_risk.sql
+++ sql/Module C - Production & Operations/C7_inventory_risk.sql
@@ -1,2 +1,12 @@
 --Objective: Identify products where stock_quantity is above the 75th percentile for their category, but units sold in the last 6 months are below the 25th percentile. These are slow-moving, high-inventory risk items. Include the estimated stock value (stock_quantity * cost_price). 
 
+--For each category compute the 75th percentile
+with percentile_per_category as(
+select 
+category_id,
+percentile_cont(0.75) within group(order by stock_quantity)
+from products p
+group by 1
+),
+
+
