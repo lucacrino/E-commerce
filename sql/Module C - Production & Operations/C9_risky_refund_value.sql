a8dce48779d5f44249b0d6e7a5bc5c308a22beb8
Luca Crinò <crinoluca@gmail.com>
2026-04-08 21:55:16 +0200

    Update C9_risky_refund_value.sql

--- sql/Module C - Production & Operations/C9_risky_refund_value.sql
+++ sql/Module C - Production & Operations/C9_risky_refund_value.sql
@@ -1,4 +1,4 @@
---Objective: For each supplier, calculate the total refund amount issued on their products, the refund rate as a percentage of total revenue, and rank suppliers by refund exposure. Then compute a 3-month rolling refund total to identify whether specific suppliers have worsening refund trends.
+--Objective: For each supplier, calculate the total refund amount issued on their products, the refund rate as a percentage of total revenue and rank suppliers by refund exposure. Then compute a rolling refund total to identify whether specific suppliers have worsening refund trends.
 
 
 --Revenues and refunds per supplier
