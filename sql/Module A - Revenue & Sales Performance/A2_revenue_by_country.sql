d3226f552aef9bad7609a6b6b74fcb339156cd3c
Luca Crinò <crinoluca@gmail.com>
2026-04-10 15:56:54 +0200

    Update A2_revenue_by_country.sql

--- sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
+++ sql/Module A - Revenue & Sales Performance/A2_revenue_by_country.sql
@@ -9,6 +9,6 @@ from
 customers c
 natural join orders o
 left join order_items oi on o.order_id = oi.order_id
-where delivery_date is not null  
+where o.status not in ('cancelled','returned')  
 group by 1,2
 order by 1, array_position(array['platinum','gold','silver','bronze'],loyalty_tier)
