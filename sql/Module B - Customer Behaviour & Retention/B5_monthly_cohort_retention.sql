8784187e3e635e948658544eae71254bd7833703
Luca Crinò <crinoluca@gmail.com>
2026-05-02 17:02:43 +0200

    Update B5_monthly_cohort_retention.sql

--- sql/Module B - Customer Behaviour & Retention/B5_monthly_cohort_retention.sql
+++ sql/Module B - Customer Behaviour & Retention/B5_monthly_cohort_retention.sql
@@ -1,4 +1,4 @@
---Objective: Group customers by the month of their first order (cohort). For each cohort, track how many customers placed at least one order in each subsequent month (months 0, 1, 2, ... from cohort month). Express retention as a percentage of the original cohort size.
+--Objective: Group customers by the month of their first order (cohort). For each cohort, track how many customers placed at least one order in each subsequent month (months 0, 1, 2, ... from cohort month).
 
 
 --For each customer select the timestamp of the first order
